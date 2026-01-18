cb747e82c5aa675137c304d8e87789f06ad59e85
Tolulope Ajayi <tolulopecynthiaajayi@gmail.com>
2026-01-18 06:04:20 +0100

    feat: Add default page title, meta description, and favicon to the application.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,17 @@ import '@/styles/globals.css';
 import { AssessmentProvider } from '@/context/AssessmentContext';
 import { AuthProvider } from '@/context/AuthContext';
 
+import Head from 'next/head';
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <AuthProvider>
             <AssessmentProvider>
+                <Head>
+                    <title>TRB Alchemy™️ | Digital Psychometric Platform</title>
+                    <meta name="description" content="Uncover your professional essence." />
+                    <link rel="icon" href="/images/logo-orange-nobg.png" />
+                </Head>
                 <Component {...pageProps} />
             </AssessmentProvider>
         </AuthProvider>
